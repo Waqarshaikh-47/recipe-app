49a68125342f5df1233ed4bd9297871b585bdc7b
Waqar Shaikh <waqarshaikh789@gmail.com>
2024-06-17 02:59:57 +0530

    Added focus on search input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,16 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import styles from "./search.module.css"
 const URL = 'https://api.spoonacular.com/recipes/complexSearch';
 const APIKEY = '068f9bf56c71414cb7d8e5dfd75bd5cc'
 export default function Search(props){
     const { foodData, setFoodData } = props
     const [query, setQuery] = useState('pizza')
-
+    const inputRef = useRef(null); // ref => { current: null }
+    useEffect(()=>{
+        if(inputRef.current){
+            inputRef.current.focus()
+        }
+    })
     useEffect(()=>{
         async function fetchData(){
             try{
@@ -20,7 +25,7 @@ export default function Search(props){
     },[query])
     return (
         <div className={styles.searchContainer}>
-            <input className={styles.input} onChange={(event)=>setQuery(event.target.value)} value={query} type="text" />
+            <input ref={inputRef} className={styles.input} onChange={(event)=>setQuery(event.target.value)} value={query} type="text" />
         </div>
     )
 }
